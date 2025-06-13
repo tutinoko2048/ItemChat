27dd8fb567db1e503931601c102739ce2e4218da
RetoRuto9900K <tutinoko2048@gmail.com>
2025-06-13 22:29:15 +0900

    init

---
+++ src/client/java/com/github/tutinoko2048/itemchat/client/ItemChatClientClient.java
@@ -0,0 +1,10 @@
+package com.github.tutinoko2048.itemchat.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemChatClientClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/github/tutinoko2048/itemchat/ItemChatClient.java
@@ -0,0 +1,10 @@
+package com.github.tutinoko2048.itemchat;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ItemChatClient implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
